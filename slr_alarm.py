ccd946f3b22e6b372ce423bfc7dc66f2199f3395
Colburn Hayden <colburn.b.hayden@gmail.com>
2015-12-19 18:16:37 -0600

    Initial Commit

---
+++ slr_alarm.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+import time
+import subprocess 
+
+slr_log_file='/root/scripts/slr_1.log'
+slr_list = ''
+
+slr_sleep_log='/opt/callrec/logs/sleeplog.log'
+sleep_log = open(slr_sleep_log, 'a')
+sleep_log.close()
+create=''
+sip=''
+
+
+def readLog():
+  global slr_list, slr_log_file
+  slr_log = open(slr_log_file, 'r')
+  slr_list = list(slr_log)
+  slr_log.close()
+
+
+def getPID():
+  global SLR, slrpid, slrerr
+  SLR = subprocess.Popen(['/sbin/pidof', 'slr'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  slrpid, slrerr = SLR.communicate()
+
+def findCreate():
+  global create, slr_sleep_log, slr_list
+  readLog()
+  getPID()
+  for i in slr_list[-200:-1]:
+    if 'CREATE' in i:
+      create = 1
+      break
+    else:
+      create = 0
+
+
+def findSipApp():
+  readLog()
+  global sip, slr_list
+  for i in slr_list[-200:-1]:
+    if 'sip.app' in i:
+      sip = 1
+      break
+    else: 
+      sip = 0
+      continue
+
+
+def writeLog(x):
+  global slr_sleep_log, sleep_log
+  sleep_log = open(slr_sleep_log, 'a')
+  sleep_log.write(time.ctime(time.time()) + x)
+  sleep_log.close()
+
+def restart():
+  restartSlr=subprocess.call(['/opt/callrec/bin/rc.callrec_slr', 'restart'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  getPID()
+  writeLog(' INFO: slr restarted due to silence. Exiting... pid=' + slrpid)
+  quit()
+
+def checkMsgs():
+  global sleep_log, sip, create
+  findCreate()
+  findSipApp()
+  counter=0
+  while counter < 3:
+    if sip==1 and create==1:
+      getPID()
+      writeLog(' INFO: slr is running fine. Exiting... pid=' + slrpid)
+      quit()
+    elif sip==0 and create==1:
+      time.sleep(5)
+      writeLog(' INFO: no sip packets found, trying again... pid=' + slrpid) 
+      findSipApp()
+      counter +=1
+    else:
+      writeLog(' INFO: No create messages found. Exiting.... pid=' + slrpid)      
+      quit()
+  getPID()
+  writeLog(' ERROR: slr is silent. Will be restarted. pid=' + slrpid)
+  restart() 	 
+
+checkMsgs()
+
---
+++ whisper_repair.py
@@ -0,0 +1,61 @@
+
+import pg, subprocess                                                                                                                                                                                       
+callrec = pg.connect('callrec', 'localhost', 5432, None, None, 'callrec', 'callrec')                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                                                                      
+whisper=''                                                                                                                                                                                                  
+callrecdict=''                                                                                                                                                                                              
+created_ts="select distinct cplid, created_ts from couple_extdata where cplid='{0}' order by cplid, created_ts asc;"                                                                                        
+count=callrec.query("select count(*) from couple_extdata where value in (select value from couple_extdata group by value having count(value) > 2) and key='JTAPI_CISCO_ID';").dictresult()[0]['count']      
+n=0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+offset=0                                                                                                                                                                                                    
+processed=0                                                                                                                                                                                                 
+log=''                                                                                                                                                                                                      
+log1=''                                                                                                                                                                                                     
+def findCalls():                                                                                                                                                                                            
+  global whisper, callrecdict, offset                                                                                                                                                                       
+  whisper="select * from couple_extdata where value in (select value from couple_extdata group by value having count(value) > 2) and key='JTAPI_CISCO_ID' order by value, cplid desc limit 40 offset {0};".f
+ormat(offset)                                                                                                                                                                                               
+  callrecdict=callrec.query(whisper).dictresult()                                                                                                                                                           
+  print "Finding calls: {0}".format(whisper)
+
+
+
+
+def processWhisper():                                                                                                                                                                                       
+  global n, m, cplid, callrecdict, log, log1                                                                                                                                                                
+  for i in callrecdict:                                                                                                                                                                                     
+    if n < len(callrecdict):                                                                                                                                                                                
+      cplids=[callrecdict[n]]                                                                                                                                                                               
+      for z in callrecdict:                                                                                                                                                                                 
+        if z['value'] == cplids[0]['value']:                                                                                                                                                                
+          cplids.append(z)                                                                                                                                                                                  
+                                                                                                                                                                                                            
+      for cpl in cplids:                                                                                                                                                                                    
+        for cpls in cplids:                                                                                                                                                                                 
+          if cpls['cplid'] != cpl['cplid']:                                                                                                                                                                 
+            keys=callrec.query("select key,value from couple_extdata lo where cplid='{0}' and not exists (select key from couple_extdata hi where cplid='{1}' and lo.key=hi.key);".format(cpls['cplid'], cpl
+['cplid'])).dictresult()                                                                                                                                                                                    
+            keys2=callrec.query("select key,value from couple_extdata low where cplid='{0}' and not exists (select key from couple_extdata high where cplid='{1}' and low.key=high.key);".format(cpl['cplid'
+], cpls['cplid'])).dictresult()                                                                                                                                                                             
+            if len(keys) != 0:                                                                                                                                                                              
+              for y in keys:                                                                                                                                                                                
+                callrec.query("insert into couple_extdata (cplid, key, value, created_ts) values ('{0}', '{1}', $${2}$$, '{3}');".format(cpl['cplid'], y['key'], y['value'], str(callrec.query(created_ts.fo
+rmat(cpl['cplid'])).dictresult()[0]['created_ts'])))                                                                                                                                                        
+                #print "Added Data: {0}:{1} to couple: {2}".format(y['key'], y['value'], cpl['cplid'])                                                                                                       
+                                                                                                                                                                                                            
+            if len(keys2) != 0:                                                                                                                                                                             
+              for d in keys2:                                                                                                                                                                               
+                callrec.query("insert into couple_extdata (cplid, key, value, created_ts) values ('{0}', '{1}', $${2}$$, '{3}');".format(cpls['cplid'], d['key'], d['value'], str(callrec.query(created_ts.f
+ormat(cpls['cplid'])).dictresult()[0]['created_ts'])))                                                                                                                                                      
+                #print "Added Data: {0}:{1} to couple: {2}".format(d['key'], d['value'], cpls['cplid'])                                                                                                                                                                                                            
+                                                                                                                                                                                  
+    n+=1                                                                                                                                                                                                    
+    m+=0                                                                                                                                                                                                    
+                                                                                                                                                       
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+while count >= 0:                                                                                                                                                                                           
+  findCalls()                                                                                                                                                                                               
+  processWhisper()                                                                                                                                                                                          
+  count=count-40                                                                                                                                                                                            
+  offset+=40   
