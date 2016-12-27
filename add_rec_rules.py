c95b369cabe27121b72650c916d1721f83cfa739
Colburn <colburn.b.hayden@gmail.com>
2016-12-27 10:47:41 -0600

    Update add_rec_rules.py

--- add_rec_rules.py
+++ add_rec_rules.py
@@ -20,26 +20,31 @@
 
 
 
-
 import pg
 
 #variable assignments.
+
 userid=1
 roleid=1
 isactive='t'
 weekdays=1111111
 starttime=0
 stoptime=86400
-numbermask='>{0}???????' #mask of recording rules. 
+numbermask='>{0}???????' #mask of recording rules.
 probability=100
 policy='NO_RECORD'
 factory='PHONE'
+
+screenrec='false' #does rule apply to screens?
+scr_prob=100
 
 #Define list of areacodes
 areacodefile='./areacodes.txt'
 
 #Rec Rules insert command
-add_rules="insert into rec_rules (id, userid, roleid, isactive, weekdays, starttime, stoptime, numbermask, priority, probability, policy, factory) values (nextval('seq_rec_rules'), {0}, {1}, '{2}', '{3}', '{4}', '{5}', '{6}', {7}, '{8}', '{9}', '{10}');"
+add_rules="insert into rec_rules (id, userid, roleid, isactive, weekdays, starttime, stoptime, numbermask, priority, probability, policy, factory) values (nextval('seq_rec_rules'), {0}, {1}, '{2}', '{3}', '{4}', '{5}', '{6}', {7}, '{8}', '{9}', '{10}') returning id;"
+
+add_rules_process="insert into rec_rules_process (ruleid, type, value) values ({0}, 'INTERNAL_DATA_MODIFICATOR', '{1} = {2}');"
 
 #connect to DB
 callrec = pg.DB('callrec', 'localhost', 5432, None, None, 'callrec', 'callrec')
@@ -59,5 +64,8 @@ else:
 
 #iterate over extensions and insert them into the DB
 for code in areacodes:
-  callrec.query(add_rules.format(userid, roleid, isactive, weekdays, starttime, stoptime, numbermask.format(code), priority, probability, policy, factory))
+  new_id=callrec.query(add_rules.format(userid, roleid, isactive, weekdays, starttime, stoptime, numbermask.format(code), priority, probability, policy, factory)).dictresult()
   priority=priority-1
+  if screenrec == 'true':
+    callrec.query(add_rules_process.format(new_id[0]['id'], 'SRS_RECORD_FLAG', 'true'))
+    callrec.query(add_rules_process.format(new_id[0]['id'], 'SRS_RECORD_PROBABILITY', scr_prob))
