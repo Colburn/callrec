c0f6218e0371a171edca4df7bab85a5aeefe4406
Colburn <colburn.b.hayden@gmail.com>
2017-09-05 12:43:42 -0500

    Create delete_na_survey.sh

---
+++ delete_na_survey.sh
@@ -0,0 +1,12 @@
+DB_ADDR="10.9.153.100"
+QUERY="delete from evaluations where evaluationid not in (select evaluationid from evaluations where evaluationid in (
+        select evaluationid from criteria where criteriaid in (
+                select criteriaid from subevaluation where subevaluationid in (
+                        select subevaluationid from evalanswers where answerid in (
+                                select answerid from answers where compliance!='NOT_APPLICABLE')
+                        )
+                )
+        )) and evalstatus='FINISHED' and qformid in (
+select qformid from questforms where qftype='SURVEY');"
+
+psql -U postgres callrec -h "${DB_ADDR}" -c "${QUERY}";
