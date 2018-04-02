72db7002950de657545c07df64f609ae0e67b485
Colburn <colburn.b.hayden@gmail.com>
2018-04-02 17:30:11 -0500

    Create add_sc_login_to_enc.py

---
+++ add_sc_login_to_enc.py
@@ -0,0 +1,31 @@
+#DB Variables
+
+import pg
+
+callrecDbName='callrec'
+callrecDbUser='wbsc'
+callrecDbPasswd='callrec'
+callrecDbHost='localhost'
+callrecDbPort=5432
+wbsc=pg.DB(callrecDbName, callrecDbHost, callrecDbPort, None, None, callrecDbUser, callrecDbPasswd)
+
+
+encDbName='encourage'
+encDbUser='encourage'
+encDbPasswd='encourage'
+encDbHost='localhost'
+encDbPort=5432
+enc=pg.DB(encDbName, encDbHost, encDbPort, None, None, encDbUser, encDbPasswd)
+
+
+
+sc_users=wbsc.query('select * from sc_users').dictresult()
+
+
+for i in sc_users:
+    enc.query("update users set callCentreId={0} where username={0}".format(i['login']))
+
+
+
+
+
